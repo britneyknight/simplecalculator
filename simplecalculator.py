062cf083923489035b16dbaa5f8f4846fb0511bc
Britney Knight <bitneyknight08@gmail.com>
2023-10-06 13:49:48 +0300

    commit

---
+++ simplecalculator.py
@@ -0,0 +1,9 @@
+#Britney knight. SCT211-0046
+r= input("Enter your name :")
+print("hello ",r)
+a=int(input("enter a number: "))
+b=int(input("enter another number: "))
+
+sum=a+b
+print("the sum of the two numbers is", sum)
+
